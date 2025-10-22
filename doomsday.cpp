b44afde8bce564c177061de3b30522f93db2f3f2
josh <joshuaholman5@gmail.com>
2025-10-21 21:38:44 -0700

    argparse bugfix

--- doomsday.cpp
+++ doomsday.cpp
@@ -145,7 +145,7 @@ int main(int argc, char **argv) {
   }
 
   // argparse loop
-  for (int i = 0; i < argc; i++) {
+  for (int i = 1; i < argc; i++) {
     std::string arg = argv[i];
 
     if (arg == "--help" || arg == "-h") {
